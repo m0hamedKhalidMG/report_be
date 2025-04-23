1e4b2acffccaeabb04aea04ff90816ffd7da2708
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2025-04-23 06:49:13 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const reportController = require('../controllers/reportController');
-const authenticate = require('../middlewares/authMiddleware');
+const {authenticateLawyer,authenticate} = require('../middlewares/authMiddleware');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.put('/user', authenticate, reportController.updateUser);
 router.get('/profile', authenticate, authController.getUserData);
+router.post('/registerLawyer', authController.registerLawyer);
 
 module.exports = router;

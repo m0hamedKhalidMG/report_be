0dc98c2477e61c10b94ecfc4395b4a8cb1cdc349
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2025-04-23 07:01:06 +0200

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const reportController = require('../controllers/reportController');
-const authenticate = require('../middlewares/authMiddleware');
+const {authenticateLawyer,authenticate} = require('../middlewares/authMiddleware');
 
 router.put('/', authenticate, reportController.updateUser);
 
